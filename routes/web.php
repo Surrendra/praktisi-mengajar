9d783d35c4d6ffe6ca51925e7acdfe26d2a177fa
Surendra Made <zuren.dra96@gmail.com>
2024-04-17 13:54:29 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sample-view', function () {
+    return view('sample-view');
+});
