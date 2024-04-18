78fcbd79d00a447672804e5134a468ff7b5540f4
Surendra Made <zuren.dra96@gmail.com>
2024-04-18 10:21:46 +0800

    Add route for sample-bootstrap view

--- resources/views/sample-view.blade.php
+++ resources/views/sample-view.blade.php
@@ -1,19 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
 </head>
 <body>
-
-{{-- <script src="{!! asset('vendors/jquery/jquery-3.7.1.min.js') !!}"></script> --}}
+<div id="myElementId" class="myClass">
+    <h3 class="myClass"></h3>
+    <p>Ini adalah program dengan jquery</p>
+    <p>dan Laravel</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non illo, libero reiciendis dicta nobis incidunt explicabo, quia in provident nesciunt iusto cum. Iusto expedita cumque quas labore tempore obcaecati eos?</p>
+</div>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script>
-$(document).ready(function () {
-    console.log('Hellow Jquery');
+$(document).ready(async function () {
+    let status = loadData().then((res) => {
+        loadAnimate();
+    });
 });
+
+async function loadData() {
+    // count down
+    for (let i = 3; i >= 0; i--) {
+        console.log(i);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+    return 'done';
+}
+
+function loadAnimate() {
+    $("#myElementId").animate({
+        opacity: 0.5,
+        left: "+=50",
+        height: "toggle"
+    }, 1000);
+}
+
+
 </script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/sample-view', function () {
     return view('sample-view');
 });
+
+Route::get('/sample-bootstrap', function () {
+    return view('bootstrap-view');
+});
