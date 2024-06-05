4480d4e8ff60f4d7c3764e4807feb829c46b36f8
Surendra Made <zuren.dra96@gmail.com>
2024-06-05 15:12:47 +0800

    feat: Add JSON endpoint for student gender count

--- routes/testing.php
+++ routes/testing.php
@@ -7,37 +7,21 @@ use App\Http\Controllers\TransactionController;
 
 
 Route::get('/json', function () {
-    $products = json_decode(file_get_contents(storage_path('data/products.json')), true);
-    $products = collect($products);
-    // select where name like will return array
-    // $products = $products->where('name', 'like', '%will%');
-    // dd($products);
+    $json_students = file_get_contents(storage_path('data/students.json'));
+    $array_students = json_decode($json_students,true);
+    $students = collect($array_students);
 
 
-    // count the collection group by category
-    // $products = $products->groupBy('category');
-    // $products = $products->map(function($item){
-    //     return $item->count();
-    // });  
-
-    $students = json_decode(file_get_contents(storage_path('data/students.json')), true);
-    $students = collect($students);
-
-
-    // group by gender and count
-    // $students = $students->groupBy('gender');
-    // $students = $students->map(function ($item) {
-    //     return $item->count();
-    // });
-
-    // add new total grade using map
-    $students = $students->map(function ($item) {
-        $item['total_grade'] = ($item['maths'] + $item['physics'] + $item['english']) / 3;
-        return $item;
+    $students = $students->groupBy('gender')->map(function ($group) {
+        return $group->count();
     });
-
-    $students = $students->where('total_grade', '>', 80);
     
-    // $request->url()
+
+    return [
+        'group_by' => $students,
+        
+    ];
+    
+
 });
 
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,58 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\TransactionController;
+use Illuminate\Support\Str;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/testing/collection', function () {
+    $json_products = file_get_contents(storage_path('data/products.json'));
+    $array_products = json_decode($json_products,true);
+    $products = collect($array_products);
+
+
+    $products->push([ #push
+        "id" => Str::random(50),
+        "name" => "New Product from collection",
+        "description" => "This is a new product from collection",
+        "features" => ["New feature 1", "New feature 2"],
+        "price" => 100000,
+        "keywords" => ["New", "Product"],
+        "url" => "https://example.com/new-product",
+        "category" => "New Category",
+        "subcategory" => "New Subcategory",
+    ]);
+    return $products;
+
+
+    $last_data = $products->last();
+    $first_data = $products->first();
+    $count_data = $products->count();
+    $map_products = $products->map(function ($product) {
+        $product['slug'] = Str::slug($product['name']);
+        $tax = 0.1 * $product['price'];
+        $product['tax'] = $tax;
+        $product['price_after_tax'] = $product['price'] + $tax;
+        return $product;
+    });
+
+    $pluck_products = $products->pluck('category'); # pluck
+    $product_by_categories = $products->filter(function ($product) { #filter
+        return Str::contains($product['name'], 'Google'); 
+    });
+
+    
+    return [
+        'count_data' => $count_data,
+        'product_by_categories' => $product_by_categories,
+        'pluck_products' => $pluck_products,
+        'map_products' => $map_products,
+        'last_data' => $last_data,
+        'first_data' => $first_data,
+        
+    ];
 });
 
 
